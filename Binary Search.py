643c20e905a846d5d279d5f67ed3fa8fa682f373
Emmanuel Anya Ibem <60658282+EmmanuelIbem@users.noreply.github.com>
2021-12-18 14:29:58 -0600

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -1,1 +1,25 @@
+# I will be implementing a binary search in python with comments to explain each step. I will also throw in frequent uses.
+
+# This is the iterative implementation of binary search
+
+def find_index(elements,value):
+    left = 0       #set the lowerbound to index 0
+    right = len(elements) -1   #set the upperbound to the highest index
+
+    while left <= right:
+        middle = (left + right) // 2    # we define the middle as the average between the left and right
+
+        # This is how we find the value we are looking for as well as updating the upper and lower bound
+
+        if elements[middle] == value:
+            return middle
+        
+        if elements[middle] < value:
+            left = middle + 1   # if the value is greater than the middle, we move up our lower bound
+        elif elements[middle] > value:
+            right = middle -1   # If the value is smaller than the upper bound, reduce the upper bound from the middle
+    
+ 
+
+
 
